f195810b80c827db87091b109b8dfd3d9172de2b
Christian Flanders <chflanders@gmail.com>
2017-08-26 20:37:57 -0700

    Added activity indicator cocoa pod

--- FoodTinder/Controller/WaitingScreenViewController.swift
+++ FoodTinder/Controller/WaitingScreenViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
+
+
 class WaitingScreenViewController: UIViewController {
     //MARK: Enums
     
@@ -17,6 +20,7 @@ class WaitingScreenViewController: UIViewController {
     //MARK: Outlets
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var workingLabel: UILabel!
+    @IBOutlet weak var activity: NVActivityIndicatorView!
     
     //MARK: Weak Vars
     
@@ -32,7 +36,10 @@ class WaitingScreenViewController: UIViewController {
     //MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        requestRestaurantsFromYelp(lattitude:lattitude, longitude:longitude, radius: distanceInMiles, offset: "0") { data in
+        activity.type = .ballTrianglePath
+        activity.startAnimating()
+        DispatchQueue.global().async {
+        requestRestaurantsFromYelp(lattitude:self.lattitude, longitude:self.longitude, radius: self.distanceInMiles, offset: "0") { data in
             var progress:Float = 0.0
             for i in 0..<data.businesses.count{
                 self.businessArray.append(data.businesses[i])
@@ -42,10 +49,12 @@ class WaitingScreenViewController: UIViewController {
             self.imageArray = downloadAllImagesFor(self.businessArray)
             self.performSegue(withIdentifier: "FinishedDownloadingSegue", sender: nil)
         }
+        }
         print("latitude is \(lattitude) and longitude is \(longitude)")
         // Do any additional setup after loading the view.
     }
-    
+    override func viewDidAppear(_ animated: Bool) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -55,8 +64,8 @@ class WaitingScreenViewController: UIViewController {
     
     //MARK: Instance Methods
 
-    
-    
+ 
+
      // MARK: - Navigation
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
